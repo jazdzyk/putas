8d687887cd01a0d64004d11261908100f1eff7da
dczesak <czesak.dominik99@gmail.com>
2022-09-23 21:49:23 +0200

    updated decorator

--- decorator/timer.py
+++ decorator/timer.py
@@ -1,23 +1,33 @@
 import time
+import datetime
 
-def decorator(function):
-    def wew(*args, **kwargs):
-        start = time.time()
-        x = function(*args, **kwargs)
-        end = time.time()
-        print(function.__name__, "execution time", end - start, "sekund.")
-        return x
-    return wew
 
-@decorator
-def add(a,b):
+def timer(function_name, with_date=False):
+    def wrap(function):
+        def inner(*args, **kwargs):
+            if with_date:
+                date = datetime.datetime.now()
+                print(f"Today is: {date}")
+            start = time.time()
+            x = function(*args, **kwargs)
+            end = time.time()
+            print(f"{function_name} execution time {end - start} seconds.")
+            return x
+        return inner
+    return wrap
+
+
+@timer(function_name="Adding", with_date=True)
+def add(a, b):
     time.sleep(1)
-    return a+b
+    return a + b
 
-@decorator
-def multiply(a,b):
+
+@timer(function_name="Multiplying")
+def multiply(a, b):
     time.sleep(1)
-    return a*b
+    return a * b
 
-print(add(2,2333452))
-print(multiply(24,38))
+
+print(add(2, 2333452))
+print(multiply(24, 38))
